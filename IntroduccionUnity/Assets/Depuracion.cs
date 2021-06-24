1830129f72a3853102789a5d6fd5dcaaf6e88bab
ibooio <iboosocial@gmail.com>
2021-06-24 00:50:37 -0300

    script - texturas

---
+++ IntroduccionUnity/Assets/Depuracion.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Depuracion : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("bienvenido1");
+        Debug.Log("bienvenido2");
+        Debug.Log("bienvenido3");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
